6a0abffd19f377ef2430370372fced2a505f9981
Divya354 <dsk5df@virginia.edu>
2017-03-13 19:40:10 -0400

    automate try 2

---
+++ src/automate.sh
@@ -0,0 +1,13 @@
+rm -rf ref/
+rm -rf output/
+mkdir ref
+mkdir output
+for i in testcases/*.cl
+do
+bn=$(basename $i)
+filename="${bn%%.*}"
+./cool $i --class-map --out output/$filename
+./cool $i --parse --out ref/$filename
+node main.js ref/"$filename.cl-ast"
+diff output/"$filename.cl-type" ref/"$filename.cl-type"
+done
---
+++ src/testcases/test.cl
@@ -0,0 +1,5 @@
+class Main inherits IO {
+	x : Int <- 666;
+	y : Object <- 999;
+	main() : Object { 777 };
+};
