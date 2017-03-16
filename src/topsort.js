8879f10b3dfe1aeda7cd712ebf10ee2c4c72bbc7
Anna Metrinko <am2jm@virginia.edu>
2017-03-16 19:30:53 -0400

    fixes sort?2

--- src/topsort.js
+++ src/topsort.js
@@ -1,69 +1,69 @@
 // var graph = [[1,2],[2,3],[3,4],[5,6],[5,9],[9,10]];
 // #dictionary of nodes -> outgoing nodes
 module.exports = {
- sortTopo : function(graph){
-  var x = {};
-  // #dictionary of nodes -> incoming nodes
-  var y = {};
-  // #list of starting nodes (no incoming edges)
-  var start_nodes = [];
+	 sortTopo : function(graph){
+		  var x = {};
+		  // #dictionary of nodes -> incoming nodes
+		  var y = {};
+		  // #list of starting nodes (no incoming edges)
+		  var start_nodes = [];
 
-  // #loop through input 2 at a time
-  for (var i =0; i < graph.length; i++){
-      var start = graph[i][0];
-      var end = graph[i][1];
+		  // #loop through input 2 at a time
+		  for (var i =0; i < graph.length; i++){
+			  var start = graph[i][0];
+			  var end = graph[i][1];
 
-     // #remove edges that do not belong in start_nodes
-     if (!checkin(start, start_nodes)){
-         start_nodes.push(start);
-          console.log(start_nodes);
-       }
-     if (!checkin(end, start_nodes)){
-       var ind = start_nodes.indexOf(end);
-       if(ind != -1) {
-          start_nodes.splice(ind, 1);
-        }
-      }
+			 // #remove edges that do not belong in start_nodes
+			 if (!checkin(start, start_nodes)){
+				 start_nodes.push(start);
+				  console.log(start_nodes);
+			   }
+			 if (!checkin(end, start_nodes)){
+			   var ind = start_nodes.indexOf(end);
+			   if(ind != -1) {
+				  start_nodes.splice(ind, 1);
+				}
+			  }
 
-      if(!(end in y)){
-         //  #map edges to their incoming edges
-            y[end] = [start];
-          }
-      else{
-           var temp_list = y[end];
-           temp_list.push(start);
-           y[end]= temp_list;
-         }
-      if (!(start in x)){
-        //  #map edges to their outgoing edges
-          x[start] = [end];
-        }
-      else{
-          var temp_list = x[start];
-          temp_list.push(end);
-          x[start]= temp_list;
-        }
-  }
+			  if(!(end in y)){
+				 //  #map edges to their incoming edges
+					y[end] = [start];
+				  }
+			  else{
+				   var temp_list = y[end];
+				   temp_list.push(start);
+				   y[end]= temp_list;
+				 }
+			  if (!(start in x)){
+				//  #map edges to their outgoing edges
+				  x[start] = [end];
+				}
+			  else{
+				  var temp_list = x[start];
+				  temp_list.push(end);
+				  x[start]= temp_list;
+				}
+		  }
 
-  // #create list of nodes with no incoming edges edges
+		  // #create list of nodes with no incoming edges edges
 
-  for (var key in y) {
-      if( y.hasOwnProperty(key) ) {
-          start_nodes.forEach(function(snode){
-            if (snode == key){
-              var i = start_nodes.indexOf(snode);
-              if(i!=-1){
-                start_nodes.splice(i,1);
-              }
-              }
-          });
-        }
-      }
+		  for (var key in y) {
+			  if( y.hasOwnProperty(key) ) {
+				  start_nodes.forEach(function(snode){
+					if (snode == key){
+					  var i = start_nodes.indexOf(snode);
+					  if(i!=-1){
+						start_nodes.splice(i,1);
+					  }
+					  }
+				  });
+				}
+			  }
 
-  // #call topologicalSort
-  stack = [];
-  return topologicalSort(start_nodes,stack,x,y);
-},
+		  // #call topologicalSort
+		  stack = [];
+		  return topologicalSort(start_nodes,stack,x,y);
+	}
 
 };
 
