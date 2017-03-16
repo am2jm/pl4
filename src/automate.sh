843cb8732f53b164b6bc91cc1ef40f3dce1bd56a
Divya354 <dsk5df@virginia.edu>
2017-03-16 19:49:16 -0400

    automaye

--- src/automate.sh
+++ src/automate.sh
@@ -6,8 +6,9 @@ for i in testcases/*.cl
 do
 bn=$(basename $i)
 filename="${bn%%.*}"
+echo $filename
 ./cool $i --class-map --out output/$filename
 ./cool $i --parse --out ref/$filename
 node main.js ref/"$filename.cl-ast"
-diff output/"$filename.cl-type" ref/"$filename.cl-type"
+# diff output/"$filename.cl-type" ref/"$filename.cl-type"
 done
